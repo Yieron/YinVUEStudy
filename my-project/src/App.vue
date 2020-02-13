996d8495f65d59bdf18f86329097ddca762e94f0
Yieron东 <2293264218@qq.com>
2020-02-13 15:14:32 +0800

    【端】Vue 【Task】基础学习 【修改内容】 【后续跟进】

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TODOList></TODOList>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TODOList from './components/TODOList'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TODOList,
   }
 }
 </script>
---
+++ my-project/src/components/TODOItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="TODOItem">
+    <ul>
+        <li class="item" @click="handleDelete">{{content}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TODOItem',
+  props:['content','index'],
+  methods:{
+      handleDelete(){
+          this.$emit('delete',this.index)
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .item{
+        color: blue
+    }
+</style>
